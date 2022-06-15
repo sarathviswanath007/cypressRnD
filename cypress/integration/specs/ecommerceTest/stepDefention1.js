dbbbd1a563259dcbd0ee4ab386dd7e72d6731648
sarath <sarathviswanath93@gmail.com>
2022-06-15 13:36:49 +0530

    data-table

--- cypress/integration/specs/ecommerceTest/stepDefention1.js
+++ cypress/integration/specs/ecommerceTest/stepDefention1.js
@@ -1,14 +1,17 @@
 import { Given , And , Then , When} from "cypress-cucumber-preprocessor/steps";
 
 Given('I open E-commerce website',()=>{
-cy.visit('https://shop.demoqa.com/my-account/')
+cy.visit('/')
 })
-When('I add items to cart',()=>{
-cy.reload()
+And('I do the registration with valid username, email and password',(dataTable)=>{
+  const  userName = dataTable.rawTable[1][0]
+  const regEmail = dataTable.rawTable[1][1]
+  const  regPassword = dataTable.rawTable[1][2]
+cy.get('#reg_username').type(userName)
+cy.get('#reg_email').type(regEmail)
+cy.get('#reg_password').type(regPassword)
+cy.get('.woocommerce-Button').click()
 })
-And('Validate total price',()=>{
-cy.reload()
-})
-Then('Select the country submit and verify thank you message',()=>{
+And('Verify if the registration is successful or not',()=>{
 console.log('success')
 })
